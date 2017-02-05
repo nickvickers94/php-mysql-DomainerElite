dee65129371f9d50ab436195318b5cf743268a30
Jacky <jackychan19891016@outlook.com>
2017-02-05 08:42:39 +0400

    Alpha version of new design
    Signed-off-by: Jacky <jackychan19891016@outlook.com>

---
+++ members/software1.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Domainer Elite</title>
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
+    <link href="http://fonts.googleapis.com/css?family=Raleway" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=PT+Sans:400,700" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" />
+</head>
+
+<body>
+	
+    <div id="content" class="content">
+        <header id="header" class="row-1">
+        	<div id="headerdiv" class="body row-12">
+                <img id="headerimg" src="img/header.png" alt="Domainer Elite Header" class="row-12 col-12" />
+    		</div>
+        </header>
+
+        <main id="main" class="row-11">
+            <div id="maindiv" class="body row-12">
+    			<div id="softdiv" class="col-9">
+                    <div id="videodiv" class="row-4">
+                        <iframe id="video" src="https://www.youtube.com/embed/jlEk2g7eAYU?rel=0&modestbranding=1" id="i" name="i"></iframe>
+                    </div>
+
+                    <div id="domaindiv" class="row row-8">
+                        <div id="selectmode" class="col-6">
+                            <div id="domains" class="row-4 divgroup">
+                                <p class="group-name">Domains</p>
+                                <div class="group row-10">
+                                    <div class="radiocontainer">
+                                        <input type="radio" name="domain" id="expireddomains">
+                                        <label for="expireddomains"><span class="radio">Expired Domains</span></label>
+                                    </div>
+                                    <div class="radiocontainer">
+                                        <input type="radio" name="domain" id="jamiesdomains">
+                                        <label for="jamiesdomains"><span class="radio">Jaime's Domains</span></label>
+                                    </div>
+                                    <div class="radiocontainer">
+                                        <input type="radio" name="domain" id="dicdomains">
+                                        <label for="dicdomains"><span class="radio">Dictionary Domains</span></label>
+                                    </div>
+                                    <button class="button" style="width: 100px; height: 30px; margin-left: 20px; margin-bottom: 20px;">Submit</button>
+                                </div>
+                            </div>
+
+                            <div id="keywords" class="row-8 divgroup">
+                                <p class="group-name">Search Domains</p>
+                                <div class="group row-10 overscroll">
+                                    <select id="soflow-color">
+                                      <!-- This method is nice because it doesn't require extra div tags, but it also doesn't retain the style across all browsers. -->
+                                      <option>Select a Keyword</option>
+                                      <option>Keyword 1</option>
+                                      <option>Keyword 2</option>
+                                    </select>
+
+                                    <select id="soflow-color">
+                                      <!-- This method is nice because it doesn't require extra div tags, but it also doesn't retain the style across all browsers. -->
+                                      <option>Select a Starting Word</option>
+                                      <option>Word 1</option>
+                                      <option>Word 2</option>
+                                    </select>
+
+                                    <select id="soflow-color">
+                                      <!-- This method is nice because it doesn't require extra div tags, but it also doesn't retain the style across all browsers. -->
+                                      <option>Select an Ending Word</option>
+                                      <option>Word 1</option>
+                                      <option>Word 2</option>
+                                    </select>
+
+                                    <select id="soflow-color">
+                                      <!-- This method is nice because it doesn't require extra div tags, but it also doesn't retain the style across all browsers. -->
+                                      <option>Select an Extension</option>
+                                      <option>.com</option>
+                                      <option>.info</option>
+                                    </select>
+
+                                    <div class="row" style="margin: 15px;">
+                                        <div class="col-6" style="align-content: center;">
+                                            <button class="button">Search</button>
+                                        </div>
+                                        <div class="col-6" style="align-content: center;">
+                                            <button class="button">Add Keyword</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <div id="result" class="col-6 divgroup">
+                            <p class="group-name">Available Domains</p>
+                            <div id="available" class="menu group row-11 cd-faq-items">
+                                <ul id="basics" class="cd-faq-group">
+                                    <li>intensecars.com<button class="myButton">Register</button><button class="myButton">Appraise</button></li>
+                                    <li>passivecars.com<button class="myButton">Register</button><button class="myButton">Appraise</button></li>
+                                    <li>howtodocars.com<button class="myButton">Register</button><button class="myButton">Appraise</button></li>
+                                    <li>howtolearncars.com<button class="myButton">Register</button><button class="myButton">Appraise</button></li>
+                                </ul>
+                            </div>
+                            
+                        </div>
+                    </div>                                     
+                </div>
+
+                <div class="col-3 aside">
+                    <a href="https://flippa.com/" target="_blank" class="linkbutton">Flippa</a>
+                    <a href="https://sedo.com/us/" target="_blank" class="linkbutton">Sedo</a>
+                    <a href="http://morganlinton.com/top-ten-places-to-sell-your-domain-names/" target="_blank" class="linkbutton">Morganlinton</a>
+                    <a href="http://www.godaddy.com/" target="_blank" class="linkbutton">GoDaddy</a>
+                    <a href="https://www.freemarket.com/" target="_blank" class="linkbutton">Freemarket</a>
+                    <a href="http://www.domainsherpa.com/sell-at-marketplaces/" target="_blank" class="linkbutton">Domainsherpa</a>
+                    <a href="http://www.websitebroker.com/" target="_blank" class="linkbutton">Websitebroker</a>
+                    <a href="http://www.hongkiat.com/blog/selling-domain-names/" target="_blank" class="linkbutton">Hongkiat</a>
+                    <a href="https://www.quora.com/What-are-some-good-reputable-resources-for-selling-domains" target="_blank" class="linkbutton">Quora</a>
+                    <a href="http://www.pcnames.com/articles/an-introduction-to-buying-and-selling-domain-names" target="_blank" class="linkbutton">Pcnames</a>
+                </div>
+            </div>
+
+        </main>
+    </div>
+</body>
+</html>
