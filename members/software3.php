5d9af7a334c6134a08286ac9ed104c2e6c0f18cd
Jacky <jackychan19891016@outlook.com>
2017-02-09 09:24:33 +0400

    beta version design

--- members/software3.php
+++ members/software3.php
@@ -8,6 +8,8 @@
 <?php $page = 'tables';   // To set active on the same id of primary menu ?>
 <!-- End of Data -->
 
+<link rel="stylesheet" href="css/style.css" />
+
 <?php require_once('templates/headers/'.$header.'.tpl.php'); ?>
 
 <div class="content">
@@ -40,7 +42,7 @@
                 <div class="panel widget light-widget panel-bd-top">
                   <div class="panel-heading no-title"> </div>
                   <div class="panel-body">
-                    <iframe id="video" src="https://www.youtube.com/embed/jlEk2g7eAYU?rel=0&modestbranding=1"></iframe>
+                    <iframe width="720" height="405" src="https://www.youtube.com/embed/jlEk2g7eAYU?rel=0&modestbranding=1"></iframe>
                   </div>
                 </div>
                 <!-- Panel Widget --> 
@@ -51,12 +53,90 @@
                 <div class="panel widget light-widget panel-bd-top">
                   <div class="panel-heading no-title"> </div>
                   <div class="panel-body">
-                    <iframe id="video" src="https://www.youtube.com/embed/jlEk2g7eAYU?rel=0&modestbranding=1"></iframe>
+                    <div class="row">
+                      <div class="group radiogroup" style="width: 50%; height: 300px;">
+                        <ul>
+                          <li>
+                            <input type="radio" id="f-option" name="selector">
+                            <label for="f-option">Expired Domains</label>
+                            
+                            <div class="check"></div>
+                          </li>
+                          
+                          <li>
+                            <input type="radio" id="s-option" name="selector">
+                            <label for="s-option">Jamie's Domains</label>
+                            
+                            <div class="check"><div class="inside"></div></div>
+                          </li>
+                          
+                          <li>
+                            <input type="radio" id="t-option" name="selector">
+                            <label for="t-option">Your Domains</label>
+                            
+                            <div class="check"><div class="inside"></div></div>
+                          </li>
+                        </ul>
+                        <button class="button">Submit</button>
+                      </div>
+                      <div class="group" style="width: 50%; height: 300px;">
+                        <select id="soflow-color">
+                          <option>Select a Keyword</option>
+                          <option>Keyword 1</option>
+                          <option>Keyword 2</option>
+                        </select>
+
+                        <select id="soflow-color">
+                          <!-- This method is nice because it doesn't require extra div tags, but it also doesn't retain the style across all browsers. -->
+                          <option>Select a Starting Word</option>
+                          <option>Word 1</option>
+                          <option>Word 2</option>
+                        </select>
+
+                        <select id="soflow-color">
+                          <!-- This method is nice because it doesn't require extra div tags, but it also doesn't retain the style across all browsers. -->
+                          <option>Select an Ending Word</option>
+                          <option>Word 1</option>
+                          <option>Word 2</option>
+                        </select>
+
+                        <select id="soflow-color">
+                          <!-- This method is nice because it doesn't require extra div tags, but it also doesn't retain the style across all browsers. -->
+                          <option>Select an Extension</option>
+                          <option>.com</option>
+                          <option>.info</option>
+                        </select>
+
+                        <button class="button">Add Keyword</button>
+
+                        <button class="button">Submit</button>
+                      </div>
+                    </div>
+                    
                   </div>
                 </div>
                 <!-- Panel Widget --> 
                 
               </div>
+
+              <div class="col-md-12">
+                <div class="panel widget light-widget panel-bd-top">
+                  <div class="panel-heading no-title"> </div>
+                  <div class="panel-body">
+                    <div class="menu">
+                      <ul>
+                        <li>intensecars.com<button class="myButton">Register</button><button class="myButton">Appraise</button><button class="myButton">Save</button><button class="myButton">Sell</button></li>
+                        <li>passivecars.com<button class="myButton">Register</button><button class="myButton">Appraise</button><button class="myButton">Save</button><button class="myButton">Sell</button></li>
+                        <li>howtodocars.com<button class="myButton">Register</button><button class="myButton">Appraise</button><button class="myButton">Save</button><button class="myButton">Sell</button></li>
+                        <li>howtolearncars.com<button class="myButton">Register</button><button class="myButton">Appraise</button><button class="myButton">Save</button><button class="myButton">Sell</button></li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+                <!-- Panel Widget --> 
+                
+              </div>
+
             </div>
             <!--row --> 
             
