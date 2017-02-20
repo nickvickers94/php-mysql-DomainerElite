b92021dc93a3ccc48e030e401d3c7dfce988d2f0
Jacky <jackychan19891016@outlook.com>
2017-02-20 13:31:59 +0400

    expired domain csv file is read

---
+++ admin/expired_domain.php
@@ -0,0 +1,39 @@
+<?php
+
+
+# include parseCSV class.
+require_once('parsecsv.lib.php');
+
+
+# create new parseCSV object.
+$csv = new parseCSV();
+
+
+# Example conditions:
+// $csv->conditions = 'title contains paperback OR title contains hardcover';
+// $csv->conditions = 'author does not contain dan brown';
+// $csv->conditions = 'rating < 4 OR author is John Twelve Hawks';
+// $csv->conditions = 'rating > 4 AND author is Dan Brown';
+
+$csv->conditions = 'appraised_value > 299 AND domain does not contain 0 AND domain does not contain 1 AND domain does not contain 2 AND domain does not contain 3 AND domain does not contain 4 AND domain does not contain 5 AND domain does not contain 6 AND domain does not contain 7 AND domain does not contain 8 AND domain does not contain 9';
+
+
+# Parse '_books.csv' using automatic delimiter detection.
+$csv->auto('files/domain.csv');
+
+
+# Output result.
+// print_r($csv->data);
+
+
+?>
+
+<?php $expired_domains = ""; ?>
+<?php foreach ($csv->data as $key => $row): ?>
+	<?php if (strlen($row['domain']) < 16): ?>
+		<?php
+			$expired_domains .= $row['domain'].",\n";
+		?>
+	<?php endif; ?>
+<?php endforeach; ?>
+<?php echo($expired_domains); ?>
