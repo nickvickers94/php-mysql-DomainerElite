71dd7c683720f680f5bb2b85429d5dd155a7366b
Jacky <jackychan19891016@outlook.com>
2017-03-17 02:02:10 +0400

    domainerelite pro tutorial added

---
+++ project/index.php
@@ -0,0 +1,657 @@
+<?php 
+
+	$video = '_KAehuUncqw';
+
+	$height = 540;
+
+	$width = 960;
+
+	$showButtonAtPercent = 50;
+
+?>
+
+<!DOCTYPE html>
+
+
+
+<html lang="en">
+
+
+
+<head>
+
+
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+
+
+
+<title>Domainer Elite PRO Tutorial</title>
+
+
+
+<meta name="viewport" content="width=device-width, initial-scale=1" >
+
+
+
+<link rel="shortcut icon" href="images/favicon.png" >
+
+
+
+<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel='stylesheet' type='text/css'>
+
+
+
+<link href="https://fonts.googleapis.com/css?family=Raleway:300,400,500,600,700,800" rel="stylesheet">
+
+
+
+<link href="../css/style.css" rel="stylesheet" type="text/css" >
+
+
+
+<link href="../css/responsive.css" rel="stylesheet" type="text/css" >
+
+
+
+<link href="../css/bootstrap.min.css" rel="stylesheet" type="text/css">
+
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,800" rel="stylesheet">
+
+<link href="font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet">
+
+
+
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+
+
+
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+
+
+
+
+</head>
+
+
+
+
+
+
+
+<body>
+
+
+
+<header id="header">
+
+
+
+  <div class="container text-center">
+
+
+
+    <div class="row">
+
+
+
+      <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+
+
+
+      <span class="col-lg-12 col-md-12 col-sm-12 col-xs-12"><img src="images/logodev.png" alt=""></span>
+
+		
+
+      </div>
+
+
+
+    </div>
+
+
+
+  </div>
+
+
+
+</header>
+
+
+
+
+
+
+
+<div class="clearfix"></div>
+
+
+
+
+
+
+
+
+
+
+
+<div id="vidarea-bg">
+
+
+
+  <div class="container">
+
+
+
+   
+
+
+
+        
+
+
+
+
+
+
+
+<center><div id="player"></div></center>
+
+
+
+
+
+
+
+
+
+
+
+
+
+  </div>
+
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+<div class="clearfix"></div>
+		<div id="section2">
+
+
+
+  <div class="container">
+
+
+
+    <div id="timed1" class="row">
+
+    
+
+    
+
+
+
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+
+    <h2 style="text-align: center">Have questions? call 1-800-752-8947 or email <a href="mailto:specialprojectwithjamie@gmail.com">specialprojectwithjamie@gmail.com</a></h2>
+
+    <p>&nbsp;</p>
+
+    
+
+    <h3 class="text-center">Let Jamie Build YOUR Custom Site for Only <strike>$1,997.00</strike> $997.00<br>
+							<br>
+							<a href="mailto:specialprojectwithjamie@gmail.com"><img src="images/soldoutbutton.png" alt="" width="930" height="122" border="0"></a></h3>
+						<p class="orderb text-center"><a href="http://videotornado.com/bonus2.html" class="btn btn-warning btn-lg btn-block" name="submit"><i class="fa fa-globe"></i></a><br>
+							<img src="images/blackfriday.png" /></p>
+						<div class="order">
+
+<center><p style="font-size: 24px; font-weight: bold">Let Jamie Build YOUR Custom Site for Only <strike>$1,997.00</strike> $997.00<br>
+								</p></center>
+
+<p class="orderb text-center"><a href="mailto:specialprojectwithjamie@gmail.com"><img src="images/soldoutbutton.png" alt="" width="930" height="122" border="0"></a><br>
+								<a href="http://videotornado.com/bonus2.html" class="btn btn-warning btn-lg btn-block" name="submit"><i class="fa fa-globe"></i></a></p>
+
+</div>
+						<p class="text-center"><img src="images/1.jpg" /></p>
+
+<p class="text-center"><img src="images/productsbig.png" /></p>
+
+<p class="text-center"><img src="images/2.jpg" /></p>
+
+<p style="font-size: 24px; font-weight: bold;">We will build your custom business for you including design,<br /> 
+
+custom copy and plug it into ClickBank for you!</p>
+
+<p class="text-center"><img src="images/4.jpg" /></p>
+
+
+
+<p style="font-size: 24px; font-weight: bold;">This allows you to take a step back and enjoy the great feeling of success<br />without all of the hard work involved in getting your site up and running.</p>
+
+<p class="text-center"><img src="images/5.jpg" /></p>
+
+<p class="text-center"><img src="images/7.jpg" /></p>
+
+<p style="text-align: left">We are looking to build a stand alone web based business online for a small group of members, and coach them in addition to the custom project.</p>
+
+<p style="text-align: left">You can consider it a joint venture and we both contribute to the project and Jamie makes sure to build the entire website, including the incredible modern graphics, everything else needed for a profit producing website.</p>
+
+<br />
+
+<ul class="fa-ul" style="text-align:left">
+
+  <li><i class="fa-li fa fa-arrow-circle-right"></i>You will receive a <strong>COMPLETE</strong> online business</li>
+
+  <li><i class="fa-li fa fa-arrow-circle-right"></i>You will have your own <strong>UNIQUE</strong> copywriting</li>
+
+  <li><i class="fa-li fa fa-arrow-circle-right"></i>Your custom site will be completely <strong>AUTOPILOT</strong></li>
+
+  <li><i class="fa-li fa fa-arrow-circle-right"></i>Your online business will include a <strong>PRODUCT</strong> that has no competition</li>
+
+</ul>
+
+<p class="text-center"><img src="images/8.jpg" /></p>
+
+
+
+<p style="text-align: left">You won't have to do a thing, except sit back and and enjoy being waited on hand and foot. This is the opportunity everyone dreams of and your seconds away from grabbing it.</p>
+
+
+
+<p style="text-align: left">Let me show you a few examples of custom websites we have created for some of our top profit making businesses. And again these are just a few...</p>
+
+
+
+<p style="color: blue; text-decoration: underline;" class="text-center">http://www.fiftytozero.com<br />
+
+http://www.spinlikeaboss.com<br />
+
+http://www.playanythingondrums.com<br />
+
+http://www.megamusicmaker.com<br />
+
+http://www.sonicproducer.com<br />
+
+http://www.pianocoachpro.com<br />
+
+http://www.guitarhotshot.com<br />
+
+http://www.violinmasterpro.com<br />
+
+http://www.getpaidtodraw.com<br />
+
+http://www.bodyideal.com<br />
+
+http://www.dessertuniversity.com<br />
+
+http://www.beatzeria.com<br />
+
+http://www.beats365.com</p>
+
+<p class="text-center" style="color: blue; text-decoration: underline;"><img src="images/9.jpg" /></p>
+
+<p class="text-center" style="color: blue; text-decoration: underline;"><img src="images/10.jpg" /></p>
+
+<p class="text-center" style="color: blue; text-decoration: underline;"><span class="text-center" style="color: blue; text-decoration: underline;"><img src="images/11.jpg" /></span></p>
+
+<p style="text-align: left">Jamie created these sites for $10K each and the owners have made hundreds of thousands of dollars with them. It has secured their future and they will NEVER work again.</p>
+
+
+
+<p style="text-align: left">FiftyToZero is <u>#1 on Google</u> for it's biggest keyword at this time of writing: Football Coaching</p>
+
+
+
+<p style="text-align: left">SonicProducer is <u>#1 for music lessons and makes $100K a year</u>. </p>
+
+
+
+<p style="text-align: left">Jamie has created hundreds of profiting websites over the last five years.</p>
+
+
+
+<p style="text-align: left">Jamie will register a strongly appealing domain name for you, has original graphics and site design done for you, does all the niche testing, members section building and copywriting. </p>
+
+
+
+<p style="text-align: left">Amazing right?</p>
+
+<p class="text-center"><img src="images/12.jpg" /></p>
+
+<p style="text-align: left">Now are you at your computer? If so, take a look at this example: http://www.fiftytozero.com</p>
+
+<p style="text-align: left">This is the sales page. The sales copy (Which is the text) will be written by the same sales copy writer who did this site (FiftyToZero)</p>
+
+
+
+<p style="text-align: left">The graphics will be of the same quality and just so you know, this site converts incredibly and makes alot of money.</p>
+
+
+
+<p style="text-align: left">Then your graphics will be produced, sales copy written and then when the sales page and site pages are completed you will be the proud owner of a custom built money making website. Custom built by the best in the business!</p>
+
+
+
+<p style="text-align: left">It's a win win situation. Everybody benefits here, we both make money.</p>    
+
+
+
+<p style="text-align: left">Thats the magic of a joint venture. We allocate a budget, we put our heads together and we create an incredible converting niche website.</p>
+
+
+
+<p style="text-align: left">We will launch a service similar to what we are proposing to you, next year so we are looking to work with a few lucky individuals such as yourself before we commit to a business model like this.</p>
+
+
+
+<p style="text-align: left">We have the resources and thats our business, we build websites.</p>
+
+
+
+<p style="text-align: left">So we would simply allocate the budgets, make sure everyone is comfortable and get started.</p>
+
+
+
+<p style="text-align: left">You would be working with Jamie of Champ Entertainment, and we are out of Connecticut.</p>
+
+
+
+<p style="text-align: left">We will contribute financially to the project. We both go in together to make sure its the best website on the market.</p>
+
+
+
+<p style="text-align: left">Again, This website will change your life. You get the sales page, the graphics, the site, You get everything!</p>
+
+<p class="text-center"><img src="images/jvzooproductsales.png" /></p>
+
+<p class="text-center"><img src="images/logos.png" /></p>
+
+<p class="text-center"><img src="images/detotalsales.png" /></p>
+
+<p style="font-size: 24px; font-weight: bold;">This is what we will do for you:</p>
+
+<p class="text-center"><img src="images/13.png" /></p>
+
+<p>If this terminology is over your head, let me interpret:</p>
+
+<p>You will have a fully operational website created for you, and it will be affiliate driven, you do NOT need to manage anything.</p>
+
+<p class="text-center">  <img src="images/14.jpg" /><br />
+
+  <br />
+
+  
+
+</p>
+
+<p style="font-size: 48px; font-weight: bold; text-decoration: underline;">You will receive:</p>
+
+<p style="font-size: 32px; font-weight: bold;">The done-for-you machine will...</p>
+
+
+
+<ul class="fa-ul">
+
+  <li><i class="fa-li fa fa-check"></i>Custom copywriting</li>
+
+<li><i class="fa-li fa fa-check"></i>A customized complete website (EXCLUSIVE)</li>
+
+<li><i class="fa-li fa fa-check"></i>A Custom logo with a custom domain name</li>
+
+<li><i class="fa-li fa fa-check"></i>The PSD's to your graphics and doc's of your copy so you can edit them (Upon Request)</li>
+
+<li><i class="fa-li fa fa-check"></i>Your site set up on Clickbank or using Paypal</li>
+
+<li><i class="fa-li fa fa-check"></i>Traffic strategy access forever</li>
+
+<li><i class="fa-li fa fa-checkt"></i>Actual physical assistance and hands on tech support</li>
+
+<li><i class="fa-li fa fa-check"></i>A personal relationship with us</li>
+
+<li><i class="fa-li fa fa-check"></i>Everything needed to have a web-based property: Order and then we will start building your site the way you want it, while speaking with us via email through the entire process.</li>
+
+</ul>
+
+
+
+<p style="font-size: 24px; font-weight: bold;">How much will this cost?</p>
+
+
+
+<p style="text-align: left">Jamie has been paid $50K for a website before.</p>
+
+
+
+<p style="text-align: left">This plan includes every possible type of promotion, to get the most traffic, or you might want to just start out with the website and take things a bit slower.</p>
+
+
+
+<p style="font-size: 24px; font-weight: bold;">Sounds good right?</p>
+
+
+
+<p style="text-align: left">I cannot stress enough, the fact that this is a really incredible opportunity...</p>
+
+
+
+<p style="text-align: left">You will be hearing from Jamie's assistant Stephen personally. Jamie and Stephen will oversee the entire project.</p>
+
+
+
+<p style="text-align: left">We will work closely with you throughout the project.</p>
+
+
+
+<p style="font-size: 24px; font-weight: bold;">What will happen <i>after</i> you sign up?</p>
+
+
+
+
+
+<p style="text-align: left">Purchase, and then you will be redirected to a form to fill out what you want your site to look like, what niche you want, and more. This is so you can supply our team with all of your desires.</p>
+
+
+
+<p style="text-align: left">Then, we will work with you to register your domain name.</p>
+
+
+
+<p style="text-align: left">We will start producing the digital product.</p>
+
+
+
+<p style="text-align: left">So are you ready?</p>
+
+<p class="text-center"><img src="images/13.png" /></p>
+
+<p class="text-center"><img src="images/productsbig.png" /></p>
+
+
+
+
+
+
+
+<div style="visibility: visible">
+
+<div class="order">
+
+<center><p style="font-size: 24px; font-weight: bold">Let Jamie Build YOUR Custom Site for Only <strike>$1,997.00</strike> $997.00<br>
+									</p></center>
+
+
+
+<p class="orderb text-center"><a href="http://videotornado.com/bonus2.html" class="btn btn-warning btn-lg btn-block" name="submit"><i class="fa fa-globe"></i></a></p>
+
+</div>
+
+
+
+</div></div>
+
+
+
+  </div>
+
+
+
+  </div>
+
+
+
+</div>
+		<div class="clearfix">
+			<a href="mailto:specialprojectwithjamie@gmail.com"><img src="images/soldoutbutton.png" alt="" width="930" height="122" border="0"></a></div>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<p class="text-center"><br>
+						<footer id="footer"><img src="images/logodev.png" alt=""></footer></p>
+					<p class="text-center copyright"><footer id="footer">Copyright &copy; 2017 - DomainerElitePro - All Rights Reserved.</footer></p>
+				</div>
+			</div>
+		</div>
+		<div class="clearfix"></div>
+
+
+
+
+
+
+
+<div class="disclaimer">
+
+
+
+  <div class="container">
+
+
+
+    <div class="row">
+
+
+
+      <div class="col-md-12">
+
+
+
+        <p>Every effort has been made to accurately represent the product(s) sold through this website and their potential. Any claims made or examples given are believed to be accurate, however, should not be relied on in any way in making a decision whether or not to purchase. Any testimonials and examples used are exceptional results, don't apply to the average purchaser and are not intended to represent or guarantee that anyone will achieve the same or similar results. Each individual's success depends on his or her background, dedication, desire and motivation as well as other factors not always known and sometimes beyond control. There is no guarantee you will duplicate the results stated here. You recognize any business endeavor has inherent risk for loss of capital. Basically, we can't FORCE you to TAKE ACTION, so therefore we cannot promise success.</p>
+
+
+
+
+
+
+
+<p>Disclaimer: We do NOT believe in get rich quick programs. We believe in hard work, adding value and dedication to reach your goals. It's a fact that one that does not take action will see no results whatsoever. The results featured on this page are not the norm and are extraordinary results from hard work, commitment and dedication by following through and taking action. You will get no results whatsoever if you assume by soaking up information products, joining program after program your life will change with riches. This is not for you. Your results are based upon your actions. If you want a magic button that will fulfil your life with riches then please leave this page and do NOT purchase. Our products are intended to help you share your message with the world whilst growing your business. We don't make any guarantees about your own results because we don't know you. Results in life are solely based on decisions made. We are here to help and guide you to move forward faster by giving you awesome content, direction and strategies to reach your end goal. Please check the content thoroughly on this page and that you are committed to taking relentless action and will put in the effort before you decide to make a purchase. If not then please leave this page and do not purchase.</p>
+
+
+
+
+
+
+
+<p>This product does not guarantee income or success, and examples of the product owner's and other person's results do not represent an indication of future success or earnings. </p>
+
+
+
+<p class="text-center">Copyright <?php echo date('Y'); ?> - All Right Reserved | <a href="http://www.domainerelite.com/iterms.php">Terms &amp; Conditions</a> | <a href="http://www.domainerelite.com/iprivacy.php">Privacy Policy</a> | <a href="http://champsites.com">Contact</a> | <a href="http://www.domainerelite.com/members/">Paid Members</a> | <a href="http://www.domainerelite.com/jv/">Affiliates</a></p>
+
+
+
+      </div>
+
+
+
+    </div>
+
+
+
+  </div>
+
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+<script>
+
+      var tag = document.createElement('script');
+
+      tag.src = "https://www.youtube.com/iframe_api";
+
+      var firstScriptTag = document.getElementsByTagName('script')[0];
+
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+      var player;
+
+      function onYouTubeIframeAPIReady() {
+
+        player = new YT.Player('player', {
+
+          height: '<?php echo $height; ?>',
+
+          width: '<?php echo $width; ?>',
+
+          playerVars: { 'autoplay': 1, 'controls': 0, 'showinfo': 0, 'rel': 0 },
+
+          videoId: '<?php echo $video; ?>',
+
+          events: {
+
+            'onStateChange': onPlayerStateChange
+
+          }
+
+        });
+
+      }
+
+      function onPlayerStateChange(event) {
+
+        if (event.data == YT.PlayerState.PLAYING) {
+
+         	var duration = player.getDuration();
+
+         	var percent = <?php echo $showButtonAtPercent / 100; ?>;
+
+         	var timer = (duration * percent) * 1000;
+
+         	setTimeout(function(){  document.getElementById("timed").style.display = "block"; }, timer);
+
+        }
+
+      }
+
+
+
+</script>
+
+</body>
+
+
+
+</html>
